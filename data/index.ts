88761ccb9cf929ec8d0565b2438a64b2148b1a71
Saksham <s.s.birla@student.utwente.nl>
2024-08-10 17:28:54 +0200

    almost finsihed

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,11 @@
+import { iconsList } from "@tabler/icons-react";
+import { link } from "fs";
+import { title } from "process";
+
 export const navItems = [
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
-    { name: "Testimonials", link: "#testimonials" },
+    { name: "Experiences", link: "#experiences" },
     { name: "Contact", link: "#contact" },
   ];
   
@@ -72,68 +76,78 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "https://github.com/adrianhajdin?tab=repositories",
+      title: "Unreal Engine 5 Game Development",
+      des: "Exploring the world of game development with Unreal Engine 5, creating a 3D game from scratch.",
+      img: "/unrealengine.jpg",
+      iconLists: ["/ue.svg", "/c.svg", "/c++.svg"],
+      link: "https://www.youtube.com/watch?v=F4fvmBVFKGw",
     },
     {
       id: 2,
-      title: "Yoom - Video Conferencing App",
-      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-      img: "/p2.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/zoom-clone",
+      title: "Corvette C8 3D Web Animation",
+      des: "A fully interactive web animation created using the latest tech stack for one of my favorite cars, the Corvette C8.",
+      img: "/c8.png",
+      iconLists: ["/three.svg", "/tail.svg", "/re.svg", "/c.svg"],
+      link: "https://carshow1.netlify.app/",
     },
     {
       id: 3,
-      title: "AI Image SaaS - Canva Application",
-      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/ai_saas_app",
+      title: "ThreeJS Parallax Website",
+      des: "A demo Parallax Website created using Three.js, React.js, and Tailwind CSS.",
+      img: "/threejs.png",
+      iconLists: ["/three.svg", "/tail.svg", "/ts.svg", "/re.svg"],
+      link: "https://threejstutorial.netlify.app/",
     },
     {
       id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
+      title: "Anti Distraction Extension",
+      des: "A chrome extensions that block websites and apps to help you focus on your work.",
+      img: "/antidistraction.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+      link: "https://github.com/Saksham-89/Website-Blocker",
     },
+    {
+      id:5,
+      title: "Visual Integration Calculator",
+      des: "A calculator that helps you visualize the integration of functions.",
+      img:"/integration.png",
+      iconLists:['/python.svg'],
+      link:"https://github.com/Saksham-89/Integration-Calculator/tree/main",
+    },
+    {
+      id: 6,
+      title: "Product Landing Page",
+      des: "A product landing page for a Trombone company.",
+      img: "/productlanding.png",
+      iconLists:["/html.svg", "/css.svg", "/js.svg"],
+      link:"https://saksham-89.github.io/Product-Landing-Page/"
+    }
   ];
   
   export const testimonials = [
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Collaborating with a local media agency to create a booking system for their clients. The system allows clients to book appointments and services online, and the agency to manage bookings and staff schedules.",
+      name: "- Shotmaniacs",
+      title: "2024",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Creating a chat application implementing complex networking concepts such as Multi-hop routing, data encryption, and secure communication. The app allows users to chat with each other in real-time, and share files securely.",
+      name: "- University of Twente",
+      title: "2024",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Creating a dots and boxes game using Java. The game allows two players to play against each other, and the player with the most boxes wins. It also included an artificial intelligence which could play against the user.",
+      name: "- University of Twente",
+      title: "2023",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "A debit card system which uses RFID as debit cards and can be scanned using Arduino. The system ensures secure transactions and allows users to check their balance and transaction history through a web interface connected to a secure database.",
+      name: "- University of Twente",
+      title: "2023",
     },
   ];
   
@@ -145,22 +159,10 @@ export const navItems = [
       nameImg: "/cloudName.svg",
     },
     {
-      id: 2,
-      name: "appwrite",
-      img: "/app.svg",
-      nameImg: "/appName.svg",
-    },
-    {
       id: 3,
       name: "HOSTINGER",
       img: "/host.svg",
       nameImg: "/hostName.svg",
-    },
-    {
-      id: 4,
-      name: "stream",
-      img: "/s.svg",
-      nameImg: "/streamName.svg",
     },
     {
       id: 5,
@@ -173,29 +175,29 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Booking System - Shotmaniacs",
+      desc: "Collaborating with a local media agency to create a booking system for their clients. The system allows clients to book appointments and services online, and the agency to manage bookings and staff schedules.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "Chat Application",
+      desc: "Creating a chat application implementing complex networking concepts such as Multi-hop routing, data encryption, and secure communication. The app allows users to chat with each other in real-time, and share files securely.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      title: "Debit Card System",
+      desc: "A debit card system which uses RFID as debit cards and can be scanned using Arduino. The system ensures secure transactions and allows users to check their balance and transaction history through a web interface connected to a secure database.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      title: "Dots and Boxes Video Game",
+      desc: "Creating a dots and boxes game using Java. The game allows two players to play against each other, and the player with the most boxes wins. It also included an artificial intelligence which could play against the user",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
